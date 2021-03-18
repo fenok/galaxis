1b332d825438e9e5fb456b4cc23354ecee1f1038
Leonid Fenko <fenok2112@gmail.com>
2021-03-19 00:42:34 +0300

    Allow to specify cache type on useClient

--- packages/react/src/providers/ClientProvider.ts
+++ packages/react/src/providers/ClientProvider.ts
@@ -1,4 +1,4 @@
-import { Client, NonUndefined } from '@fetcher/core';
+import { Client, NonUndefined, Cache } from '@fetcher/core';
 import { useContext, useEffect, createContext, FC, createElement } from 'react';
 
 const ClientContext = createContext<Client<NonUndefined> | null>(null);
@@ -15,12 +15,12 @@ const ClientProvider: FC<ProviderProps> = ({ children, client }) => {
     return createElement(ClientContext.Provider, { value: client }, children);
 };
 
-const useClient = <C extends NonUndefined>() => {
+const useClient = <C extends NonUndefined, CACHE extends Cache<C> = Cache<C>>() => {
     const client = useContext(ClientContext);
 
     ensureClient(client);
 
-    return client as Client<C>;
+    return client as Client<C, CACHE>;
 };
 
 function ensureClient(client: Client<NonUndefined> | null): asserts client is Client<NonUndefined> {
