02cd7d3a40f3cb892c5c65a35068a2f949702dbb
Leonid Fenko <fenok2112@gmail.com>
2021-03-29 16:33:31 +0300

    Fix ResponseError check

--- packages/react-example/src/client/app/lib/isResponseError.ts
+++ packages/react-example/src/client/app/lib/isResponseError.ts
@@ -4,5 +4,5 @@ import { ResponseError } from '@fetcher/fetch';
 export function isResponseError(
     error: Error | ResponseError<ErrorResponse> | undefined,
 ): error is ResponseError<ErrorResponse> {
-    return Boolean(error && 'response' in error);
+    return error?.name === 'ResponseError';
 }
