654d1d42f2a375b3875381ab10d89e9b818f4b9c
Leonid Fenko <fenok2112@gmail.com>
2021-07-30 13:26:43 +0300

    Update publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,4 +1,5 @@
 const { execSync } = require('child_process');
+const path = require('path');
 
 function publish() {
     execSync('git checkout dev');
@@ -9,26 +10,39 @@ function publish() {
     execSync('yarn version check');
     execSync('yarn version apply --all');
 
-    const version = getVersion();
-
     execSync(`git add .`);
-    execSync(`git commit -m "Bump version to v${version}"`);
-    execSync(`git tag v${version}`);
+
+    const versions = getVersions();
+
+    execSync(`git commit -m "${getCommitMessage(versions)}"`);
+
+    versions.forEach(([packageName, version]) => {
+        execSync(`git tag ${packageName}/${version}`);
+    });
 
     execSync('git push');
     execSync('git push --tags');
 
-    execSync('yarn workspaces foreach npm publish')
+    execSync('yarn workspaces foreach npm publish --tolerate-republish');
 
     execSync('git checkout master');
+    execSync('git pull');
     execSync('git merge dev --no-ff');
     execSync('git push');
 
     execSync('git checkout dev');
 }
 
-function getVersion() {
-    return require('../package.json').version;
+function getVersions() {
+    return execSync('git diff --name-only HEAD', { encoding: 'utf8' })
+        .split('\n')
+        .filter((name) => name.includes('package.json'))
+        .map((packageName) => path.resolve(process.cwd(), packageName))
+        .map((packagePath) => [require(packagePath).name, require(packagePath).version]);
+}
+
+function getCommitMessage(versions) {
+    return `Bump versions\n\n${versions.map(([packageName, version]) => `${packageName}: ${version}`).join('\n')}`;
 }
 
 publish();
